0da99a2c452034b1d40838f7dd1119d21a9074b8
jimmywalkergit <jimmy.the.walk@gmail.com>
2016-09-25 13:36:14 -0400

    implemented main method to call beer

---
+++ src/com/company/BeerSong.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by Desktop on 9/25/2016.
+ */
+public class BeerSong {
+
+    public static void main(String[] args) {
+        // write your code here
+        //calls beer
+
+
+    }
+
+    public void Ninety_Nine_Bottles_of_Beer(){
+        //prints song
+
+
+    }
+
+
+}
